ea6eda8c1b5d89892143524d3c8623619104fef9
bernardoceub2025 <bernardo.barreto@sempreceub.com>
2026-06-13 17:16:56 -0300

    Integra Supabase ao inventario

---
+++ supabase_db.py
@@ -0,0 +1,8 @@
+from supabase import create_client
+
+
+URL = "https://lafzhdadywcwxisujygm.supabase.co"
+KEY = "sb_publishable_6JPjxH1xN1S17jxT7Yuh8Q_VBbVkshp"
+
+
+supabase = create_client(URL, KEY)
--- view.py
+++ view.py
@@ -1,60 +1,95 @@
-import sqlite3 as lite
-
-con = lite.connect('dados.db')
-
-#CRUD
+from supabase_db import supabase
 
 
-#INSERIR DADOS
+# CRUD
+
+
+# INSERIR DADOS
+# INSERIR DADOS
 def inserir_form(i):
 
-    with con:
-        cur = con.cursor()
-        query = "INSERT INTO inventario(nome, local, descricao, marca, data_compra, valor_compra, serie, imagem)VALUES(?,?,?,?,?,?,?,?)"
-        cur.execute(query,i) 
+    dados = {
+        "nome": i[0],
+        "local": i[1],
+        "descricao": i[2],
+        "marca": i[3],
+        "data_compra": i[4],
+        "valor_compra": i[5],
+        "serie": i[6],
+        "imagem": i[7]
+    }
+
+    resposta = supabase.table("inventario").insert(dados).execute()
+
+    print("RESPOSTA SUPABASE:")
+    print(resposta.data)
 
 
-#ATUALIZAR DADOS
+
+# ATUALIZAR DADOS
 def atualizar_(i):
-    with con:
-        cur = con.cursor()
-        query = "UPDATE inventario SET nome=?, local=?, descricao=?, marca=?, data_compra=?, valor_compra=?, serie=?, imagem=? WHERE id=?"
-        cur.execute(query,i) 
+
+    dados = {
+        "nome": i[0],
+        "local": i[1],
+        "descricao": i[2],
+        "marca": i[3],
+        "data_compra": i[4],
+        "valor_compra": i[5],
+        "serie": i[6],
+        "imagem": i[7]
+    }
+
+
+    supabase.table("inventario")\
+        .update(dados)\
+        .eq("id", i[8])\
+        .execute()
 
 
 
-
-#DELETAR DADOS
+# DELETAR DADOS
 def deletar_form(i):
-    with con:
-        cur = con.cursor()
-        query = "DELETE FROM inventario WHERE id=?"
-        cur.execute(query,i)
+
+    supabase.table("inventario")\
+        .delete()\
+        .eq("id", i)\
+        .execute()
 
 
-#VER DADOS
+
+# VER DADOS
 def ver_form():
-    ver_dados = []
-    with con:
-        cur = con.cursor()
-        query = "SELECT * FROM inventario"
-        cur.execute(query)
 
-        rows = cur.fetchall()
-        for row in rows:
-            ver_dados.append(row)
-    return ver_dados
+    resposta = supabase.table("inventario")\
+        .select("*")\
+        .execute()
+
+    lista = []
+
+    for item in resposta.data:
+        lista.append((
+            item["id"],
+            item["nome"],
+            item["local"],
+            item["descricao"],
+            item["marca"],
+            item["data_compra"],
+            item["valor_compra"],
+            item["serie"],
+            item["imagem"]
+        ))
+
+    return lista
 
 
-#VER DADOS INDIVIDUAIS
+
+# VER DADOS INDIVIDUAIS
 def ver_item(id):
-    ver_dados_individuais = []
-    with con:
-        cur = con.cursor()
-        query = "SELECT * FROM inventario WHERE id=?"
-        cur.execute(query,id)
 
-        rows = cur.fetchall()
-        for row in rows:
-            ver_dados_individuais.append(row)
-    return ver_dados_individuais
+    resposta = supabase.table("inventario")\
+        .select("*")\
+        .eq("id", id)\
+        .execute()
+
+    return resposta.data
